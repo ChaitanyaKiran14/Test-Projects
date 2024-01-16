0c592d9fc5686d9084234ab4752779b6d342bdf5
chaitanya <g.chaitanya1419@gmail.com>
2024-01-16 18:13:22 +0530

    final

--- src/Components/MiningPool.jsx
+++ src/Components/MiningPool.jsx
@@ -3,6 +3,30 @@ const MiningPool = () => {
         <div>
             <h1>
             Pool Data
+            <table border="1">
+            <thead>
+                <tr>
+                <th colspan="2">Total Output</th>
+                <th colspan="2">TRX</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                <td colspan="2">Valid node</td>
+                <td colspan="2">5</td>
+                </tr>
+                <tr>
+                <td colspan="2">Participant</td>
+                <td colspan="2">6</td>
+                </tr>
+                <tr>
+                <td colspan="2">Beer Revenue</td>
+                <td colspan="2">7 USDt</td>
+                </tr>
+            </tbody>
+            </table>
+
+
             </h1>
         </div>
     )
